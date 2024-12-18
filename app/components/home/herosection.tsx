3cbfe10240cfe832555be14554205d7788f7c106
alexnjoya <njoyaalexander71@gmail.com>
2024-12-18 03:17:24 +0000

    pages

--- app/components/home/herosection.tsx
+++ app/components/home/herosection.tsx
@@ -18,29 +18,29 @@ const HeroSection = () => {
                             height={100}
                             src={herShape}
                             alt="Background Shape"
-                            className="absolute inset-0 top-[450px] left-[300px]"
+                            className="absolute inset-0 top-[450px] left-[300px] hidden lg:block"
                         />
-                        <h1 className="relative text-[#000] text-[65px] lg:text-[65px] font-bold leading-[78px] mb-4 font-epilogue">
+                        <h1 className="relative text-[#000] text-[24px] lg:text-[65px] font-bold lg:leading-[70px] leading-[30px] lg:mb-4 mb-2 font-epilogue">
                             Empowering Your Business With AI and Blockchain Solutions
                         </h1>
-                        <p className="flex flex-col justify-center flex-shrink-0 text-[#444] font-epilogue text-[18px] font-medium leading-[30px] mb-6 max-w-full">
+                        <p className="flex flex-col justify-center flex-shrink-0 text-[#444] font-epilogue lg:text-[18px] text-[14px] font-medium lg:leading-[30px] leading-[20px] mb-6 max-w-full">
                             Unlock the future of digital commerce with AI-powered tools, seamless blockchain payment systems, and personalized solutions designed to boost your brand and streamline your operations.
                         </p>
-                        <div className="flex justify-center lg:justify-start space-x-4">
-                            <button className="bg-[#FE7146] text-white py-3 px-14 rounded-full hover:bg-orange-600 transition ">
-                                Register Now!
+                        <div className="flex mt-4 mb-4 lg:mb-0 justify-center lg:justify-start space-x-4">
+                            <button className="bg-[#FE7146] text-white text-[14px] lg:text-base py-1 lg:py-3 px-4 lg:px-14 rounded-full hover:bg-orange-600 transition w-3/4 sm:w-auto whitespace-nowrap">
+                                Register Now
                             </button>
-                            <Link href="/about" className="text-black py-3 text- px-6 font-bold">
+                            <Link href="/about" className="text-black py-3 text-sm lg:text-base px-6 font-bold">
                                 Learn More <strong>→</strong>
                             </Link>
                         </div>
-                        <div className="mt-10 flex items-center justify-center lg:justify-start">
+                        <div className="mt-10 lg:mt-10 flex items-center justify-center lg:justify-start">
                             <span className="text-yellow-500">★★★★★</span>
                             <span className="ml-2 text-gray-600">4.8/5 Rating</span>
                         </div>
                     </div>
                     <div className="lg:w-1/2 mt-4 lg:mt-0 relative">
-                        <div className="absolute inset-1 -top-20 -right-20">
+                        <div className="absolute inset-1 -top-20 -right-20 hidden lg:block">
                             <Image
                                 src={Gradient}
                                 alt="hero"
@@ -50,30 +50,30 @@ const HeroSection = () => {
                             />
                             <div className="absolute px-10 bottom-[-50px] left-20 bg-white p-4 rounded-full shadow-lg shadow-orange-500/20 flex items-center">
                                 <Image
-                                    src={Images} // Replace with actual path
+                                    src={Images}
                                     width={80}
                                     height={80}
                                     alt="User"
-                                    className="rounded-full "
+                                    className="rounded-full"
                                 />
                                 <span className="ml-2 text-gray-600">60 Million User Use This App</span>
                             </div>
                         </div>
-                        <div className="relative flex items-center justify-center">
-                            <div className="relative w-[300px] h-[300px] mx-auto">
+                        <div className="relative  mt-20 lg:mt-0 flex items-center justify-center">
+                            <div className="relative w-[200px] h-[200px] lg:w-[300px] lg:h-[300px] mx-auto">
                                 <Image
                                     src={heroPhone}
                                     width={678}
                                     height={681}
                                     alt="Hero"
-                                    className="absolute top-[-100px] right-[80px]"
+                                    className="absolute top-[-50px] lg:top-[-100px] right-[40px] lg:right-[80px]"
                                 />
                                 <Image
                                     src={heroIcon}
                                     width={700}
                                     height={800}
                                     alt="Hero"
-                                    className="absolute right-36"
+                                    className="absolute right-18 lg:right-36"
                                 />
                             </div>
                         </div>
