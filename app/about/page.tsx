7185a4dd675b4954c3e17a36035a33dacedc1b2d
alexnjoya <njoyaalexander71@gmail.com>
2025-01-13 18:56:53 +0000

    navigation

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Breadcrumb from "../components/common/Breadcrumb";
+import Team from "../components/common/team"
+import Partners from "../components/common/partners";
 
 
 const page = () => {
   return (
     <div>
       <Breadcrumb title="About" />
+      <Team />
+      <Partners />
      
     </div>
   );
