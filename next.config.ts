6048ce6aba0886016b825c7ab6c876774cfc37ac
alexnjoya <njoyaalexander71@gmail.com>
2025-03-15 09:31:25 +0000

    htaccess

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // output: 'standalone',
+  output: "export",
   reactStrictMode: true,
   // Required for Docker builds
   experimental: {
