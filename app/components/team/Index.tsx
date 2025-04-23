a9a8f1caed8d10890e012c3ea00d49bea99deee7
alexnjoya <njoyaalexander71@gmail.com>
2025-04-23 10:09:47 +0000

    team

--- app/components/team/Index.tsx
+++ app/components/team/Index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ProfessionalIntro from "./Introduction";
-import TeamMembers from "./Members";
 import ContactInfo from "./Contact";
-
+import CEO from "../common/team"
 const OurTeam: React.FC = () => {
   return (
     <section className="relative bg-white overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <ProfessionalIntro />
-        <TeamMembers />
+        <CEO />
         <ContactInfo />
       </div>
     </section>
